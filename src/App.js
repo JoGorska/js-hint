7480f87766c2e05b6774c6e67d8c5be4cc9f05ca
JoGorska <83362036+JoGorska@users.noreply.github.com>
2021-10-31 12:40:39 +0000

    Add Get request on click to check the expiry date of the key

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, {useState} from "react"
+
 
 import './App.css';
 import MainHeader from './components/MainHeader';
@@ -14,14 +16,36 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 
 // const resultsModal = new Modal(document.getElementById("resultsModal"))
 
-console.log(API_KEY)
-console.log(API_URL)
-
 function App() {
 
-function handleOnClick () {
-  console.log("I was clicked")
+
+
+
+  // function to handle errors https://www.tjvantoll.com/2015/09/13/fetch-and-errors/
+
+  function handleErrors(response) {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
 }
+
+function handleOnClick () {
+  
+    //full URL with the key, using the const variables from above
+    fetch(API_URL + "?api_key=" + API_KEY)
+    
+    .then(handleErrors)
+    .then (response => response.json())
+    .then (data => console.log(data.expiry))
+    
+    .then(function(response) {
+        console.log("ok");
+    }).catch(function(error) {
+        console.log(error);
+    });
+    
+};
 
   return (
     <div className="App container">
@@ -34,6 +58,6 @@ function handleOnClick () {
 
     </div>
   )
-}
+};
 
 export default App;
